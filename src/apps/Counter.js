ab11e675778a93b7838a43d42f714677753d4a54
bibashmgr <bibashthapamagar21@gmail.com>
2021-10-27 02:34:40 +0545

    Updating repo

--- src/apps/Counter.js
+++ src/apps/Counter.js
@@ -1,16 +1,44 @@
 // dependencies
 import React from 'react';
+import {useState} from 'react';
 
 // components
 import Nav from '../components/Nav';
 
+// css
+import '../styles/Counter.css';
+
 const Counter = () => {
+    const [value, setValue] = useState(0);
+
+    const handleAdd = () => {
+        setValue(value + 1);
+    }
+    const handleSub = () => {
+        setValue(value - 1);
+    }
+    const handleReset = () => {
+        setValue(0);
+    }
+    
     return (
         <>
             <Nav></Nav>
-            <div>
-                This is counter
-            </div>
+            <section className='container mt-5 text-center'>
+                <div className='h1'>
+                    Counter
+                </div>
+                <div className='mt-5'>
+                    <h1 className='value'>
+                        {value}
+                    </h1>
+                    <div className='buttons'>
+                        <button type='button' className='btn' onClick={handleSub}>Decrease</button>
+                        <button type='button' className='btn' onClick={handleReset}>Reset</button>
+                        <button type='button' className='btn' onClick={handleAdd}>Increase</button>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
