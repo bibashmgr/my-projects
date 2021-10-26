bc2f2516d4a0172e1995e611628a6c64a34963b0
bibashmgr <bibashthapamagar21@gmail.com>
2021-10-27 02:15:48 +0545

    Updating repo

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,21 +2,32 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+// components
+import {projects} from './projects';
+
+// assets
+import defaultImage from '../assets/default-image.png';
+
 const Section = () => {
     return (
         <section className='projects-container container mt-5'>
             <div className='projects-box row'>
                 <div className='project-container col-12 col-md-6 col-lg-4'>
-                    <Link to='/counter' className='project-box card shadow-sm text-decoration-none text-dark'>
-                        <div className='project-image'>
-                            <img src={require('../assets/default-image.png').default}alt="" className='card-img-top' />
-                        </div>
-                        <div className='project-body card-body'>
-                            <div className='card-title h5 text-center'>
-                                Counter
+                    {projects.map((project)=>{
+                        const {id,name,url} = project;
+                        return(
+                        <Link to={url} key={id} className='project-box card shadow-sm text-decoration-none text-dark'>
+                            <div className='project-image'>
+                                <img src={defaultImage} alt={name} className='card-img-top' />
                             </div>
-                        </div>
-                    </Link>
+                            <div className='project-body card-body'>
+                                <div className='card-title h5 text-center text-capitalize'>
+                                    {name}
+                                </div>
+                            </div>
+                        </Link>
+                        )
+                    })}
                 </div>
             </div>
         </section>
