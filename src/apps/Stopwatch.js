d19b6743af9447668f40159aed6e273a87dbd39d
bibashmgr <bibashthapamagar21@gmail.com>
2021-11-06 15:34:29 +0545

    Project 03

--- src/apps/Stopwatch.js
+++ src/apps/Stopwatch.js
@@ -1,6 +1,6 @@
 // dependencies
 import React from 'react';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 // components
 import Nav from '../components/Nav';
@@ -8,9 +8,67 @@ import Nav from '../components/Nav';
 // css
 import '../styles/Stopwatch.css';
 
+let totalms = 0;
+
 const Stopwatch = () => {
 
-    const playWatch = (e) => {
+    const [play, setPlay] = useState('active');
+    const [pause, setPause] = useState('inactive');
+    const [reset, setReset] = useState('inactive');
+
+    const [values,setValues] = useState({
+        hours:'00',
+        mins:'00',
+        secs:'00'
+    });
+
+    const {hours,mins,secs} = values;
+
+    const handleClick = (e) => {
+        if(e.target.id === 'play'){
+            setPlay('inactive');
+            setPause('active');
+            setReset('active');
+
+            window.watchStart = setInterval(() => {
+                totalms = totalms + 1000;
+
+                let textHour = Math.floor((totalms % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+                let textMin = Math.floor((totalms % (60 * 60 * 1000)) / ( 60 * 1000));
+                let textSec = Math.floor((totalms % (60 * 1000)) / (1000));
+
+                if(textHour < 10){
+                    textHour = '0' + textHour;
+                }
+                if(textMin < 10){
+                    textMin = '0' + textMin;
+                }
+                if(textSec < 10){
+                    textSec = '0' + textSec;
+                }
+
+                setValues({hours:textHour, mins:textMin, secs:textSec});
+            }, 1000);
+        }
+
+        if(e.target.id === 'pause'){
+            setPlay('active');
+            setPause('inactive');
+            setReset('active');
+
+            clearInterval(window.watchStart);
+        }
+
+        if(e.target.id === 'reset'){
+            setPlay('active');
+            setPause('inactive');
+            setReset('inactive');
+
+            clearInterval(window.watchStart);
+            setValues({hours:'00', mins:'00', secs:'00'});
+            totalms = 0;
+        }
+
     };
 
     return (
@@ -28,7 +86,7 @@ const Stopwatch = () => {
 
                     <div className='col-2 col-lg-1'>
                         <div className='row'>
-                            <div className='h1' id='div-hours'>00</div>
+                            <div className='h1' id='div-hours'>{hours}</div>
                         </div>
                         <div className='row'>
                             <label htmlFor="hours">hours</label>
@@ -36,7 +94,7 @@ const Stopwatch = () => {
                     </div>
                     <div className='col-2 col-lg-1'>
                         <div className='row'>
-                            <div className='h1' id='div-mins'>00</div>
+                            <div className='h1' id='div-mins'>{mins}</div>
                         </div>
                         <div className='row'>
                             <label htmlFor="mins">mins</label>
@@ -44,7 +102,7 @@ const Stopwatch = () => {
                     </div>
                     <div className='col-2 col-lg-1'>
                         <div className='row'>
-                            <div className='h1' id='div-secs'>00</div>
+                            <div className='h1' id='div-secs'>{secs}</div>
                         </div>
                         <div className='row'>
                             <label htmlFor="secs">secs</label>
@@ -55,13 +113,13 @@ const Stopwatch = () => {
 
                 <div className='row mt-5 justify-content-center text-center'>
                     <div className='col-2 col-lg-1 h1'>
-                        <i className='bi bi-pause-circle text-warning'></i>
+                        <i id='pause' className={'bi bi-pause-circle text-warning ' + pause} onClick={handleClick}></i>
                     </div>
                     <div className='col-2 col-lg-1 h1'>
-                        <i className='bi bi-play-circle text-success' onClick={playWatch}></i>
+                        <i id='play' className={'bi bi-play-circle text-success ' + play} onClick={handleClick}></i>
                     </div>
                     <div className='col-2 col-lg-1 h1'>
-                        <i className='bi bi-arrow-repeat text-danger'></i>
+                        <i id='reset' className={'bi bi-arrow-repeat text-danger ' + reset} onClick={handleClick}></i>
                     </div>
                 </div>
 
