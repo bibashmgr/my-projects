ef4415be3f123f63e6e5f33d574141bec108dee3
bibashmgr <bibashthapamagar21@gmail.com>
2021-11-07 05:24:04 +0545

    Updating repo

--- src/apps/DigitalClock.js
+++ src/apps/DigitalClock.js
@@ -1,5 +1,6 @@
 // dependencies
 import React from 'react';
+import {useState,useEffect} from 'react';
 
 // components
 import Nav from '../components/Nav'
@@ -8,11 +9,77 @@ import Nav from '../components/Nav'
 import '../styles/DigitalClock.css';
 
 const DigitalClock = () => {
+
+    const [values,setValues] = useState({
+        hours: '00',
+        mins: '00',
+        secs: '00'
+    });
+
+    const {hours,mins,secs} = values;
+
+    useEffect(()=>{
+        let clockInterval = setInterval(()=>{
+            let currentTime = new Date();
+            let currentHours = currentTime.getHours();
+            let currentMins = currentTime.getMinutes();
+            let currentSecs = currentTime.getSeconds();
+
+            if(currentHours < 10){
+                currentHours = '0' + currentHours;
+            }
+            if(currentMins < 10){
+                currentMins = '0' + currentMins;
+            }
+            if(currentSecs < 10){
+                currentSecs = '0' + currentSecs;
+            }
+
+            setValues({hours:currentHours,mins:currentMins,secs:currentSecs});
+            // console.log('set');
+        },1000);
+
+        return () => {
+            clearInterval(clockInterval);
+            // console.log('clear');
+        }
+    },[]);
+
     return (
         <>
             <Nav></Nav>
-            <section className='container'>
-                This is digital clock.
+            <section className='container mt-5'>
+                <div className='row text-center'>
+                    <div className='col h1'>
+                        Digital Clock
+                    </div>
+                </div>
+                <div className='row mt-5 justify-content-center text-center'>
+                    <div className='col-2 col-lg-1'>
+                        <div className='row'>
+                            <div className='h1' id='div-hours'>{hours}</div>
+                        </div>
+                        <div className='row'>
+                            <label htmlFor="hours">hours</label>
+                        </div>
+                    </div>
+                    <div className='col-2 col-lg-1'>
+                        <div className='row'>
+                            <div className='h1' id='div-mins'>{mins}</div>
+                        </div>
+                        <div className='row'>
+                            <label htmlFor="mins">mins</label>
+                        </div>
+                    </div>
+                    <div className='col-2 col-lg-1'>
+                        <div className='row'>
+                            <div className='h1' id='div-secs'>{secs}</div>
+                        </div>
+                        <div className='row'>
+                            <label htmlFor="secs">secs</label>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
